3275ac6ecb2f0f724c2399b4237292959eacf1a0
Elad Hascal <elhascal@microsoft.com>
2018-11-14 16:04:55 +0200

    2

--- app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.xamarin.testcloud.espresso.ReportHelper;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,6 +19,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ReportHelper reportHelper = com.xamarin.testcloud.espresso.Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
